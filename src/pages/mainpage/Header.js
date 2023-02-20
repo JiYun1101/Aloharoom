9f5fd9626c62d371091722051e01aa23c5888a58
gretea5 <gretea4489@gmail.com>
2023-02-20 23:07:07 +0900

    feat :  nav 마우스 커서 밑줄 생기는 기능 구현

--- src/pages/mainpage/Header.js
+++ src/pages/mainpage/Header.js
@@ -29,6 +29,12 @@ const NavGroup = styled.span`
 const NavElement = styled.span`
     margin-left: 1vw;
     margin-right: 1vw;
+    &:hover {
+        text-decoration: underline;
+        text-decoration-color: #47A5FD;
+        text-decoration-thickness: 0.15rem;
+        text-underline-offset: 0.4rem;
+    }
 `;
 
 const LogoGroup = styled.span`
