976f61d6d2cf93b60b42b3aaa0277f129a5b8eec
gretea5 <gretea4489@gmail.com>
2023-02-20 22:48:22 +0900

    feat : Banner.js 구현

--- src/pages/mainpage/Banner.js
+++ src/pages/mainpage/Banner.js
@@ -1,10 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+
+const BannerBox = styled.section`  
+    position: relative;
+    width: 100%;
+    height: 50vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const BannerImageBox = styled.div`
+    position: absolute;
+    top: 4rem;
+`;
+
+const BannerImage = styled.img`
+    width: 60rem;
+    height: 30rem;
+    border-radius: 0.5rem;
+`;
 
 const Banner = () => {
     return (
-        <div>
-            <span>banner 자리</span>
-        </div>
+        <BannerBox>
+            <BannerImageBox>
+                <BannerImage src="blue.png"/>
+            </BannerImageBox>
+        </BannerBox>
     );
 }
 
