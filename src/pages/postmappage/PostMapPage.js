12aa3d0e2862efcfc577e1b3676bc54e4e3ed34d
gretea5 <gretea4489@gmail.com>
2023-02-22 15:12:57 +0900

    feat : PostMapPage 컴포넌트 틀 생성

--- src/pages/postmappage/PostMapPage.js
+++ src/pages/postmappage/PostMapPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header";
+import PostMapSection from "./PostMapSection";
 
 const PostMapPage = () => {
     return (
     <>
         <Header/>
+        <PostMapSection/>
     </>
     );
 }
--- src/pages/postmappage/PostMapSection.js
+++ src/pages/postmappage/PostMapSection.js
@@ -1,9 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+
+const PostMapBox = styled.div`
+    position: relative;
+    width: 100%;
+    height: 50rem;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    border-color: black;
+    border-style: solid;
+`;
+
+const SearchHashTagBox = styled.div`
+    position: absolute;
+    top: 0.1rem;
+    width: 100%;
+    height: 20%;
+    border-color: green;
+    border-style: solid;
+`;
+
 
 const PostMapSection = () => {
     return (
-        <>
-        </>
+        <PostMapBox>            
+            <SearchHashTagBox>
+                
+            </SearchHashTagBox>
+        </PostMapBox>
     );
 }
 
