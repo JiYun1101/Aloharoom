9698110b8d4b55696f0f815b7cb7e6dcd77a7497
gretea5 <gretea4489@gmail.com>
2023-02-28 16:28:21 +0900

    feat : react, spring axios 연동 예시 확인
    주석 처리 했음

--- src/pages/postmappage/PostMapPage.js
+++ src/pages/postmappage/PostMapPage.js
@@ -2,7 +2,23 @@ import React from "react";
 import Header from "../Header";
 import PostMapSection from "./PostMapSection";
 
+
+// import { useEffect } from "react";
+// import axios from 'axios';
+
 const PostMapPage = () => {
+    // async function getData() {
+    //     await axios.get('http://localhost:8080/api/board')
+    //     .then((response) => {
+    //         console.log('response data : ', response.data);
+    //     })
+    //     .catch((error) => {
+    //         console.log('axios error');
+    //     });
+    // }
+    // useEffect(() => {
+    //     getData();
+    // }, []);
     return (
     <>
         <Header/>
