2fb9be0049a3a2caa2144e0fe0af85e8b66c8db5
gretea5 <gretea4489@gmail.com>
2023-03-01 21:23:24 +0900

    feat : MyPageBox 영역 생성

--- src/pages/mypage/MyPageSection.js
+++ src/pages/mypage/MyPageSection.js
@@ -1,19 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
-const MyPageBox = styled.div`
-    position: relative;
-    border-color: green;
-    border-style: solid;
+const MyPageDiv = styled.div`
+    //position: relative;
     width: 100%;
     height: 50rem;
+    border-color: green;
+    border-style: solid;
+    display: flex;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
 `;
+
+const MyPageBox = styled.div`
+    border-radius: 0.6rem;
+    border-color: #47A5FD;
+    border-style: solid;
+    width: 35rem;
+    height: 35rem;
+`;
+
+
 
 const MyPageSection = () => {
     return (
-        <MyPageBox>
-
-        </MyPageBox>
+        <MyPageDiv>
+            <MyPageBox/>
+        </MyPageDiv>
     );
 }
 
