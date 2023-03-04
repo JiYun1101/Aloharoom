a949e66cef0253b5bd2a68d4cbcb695cb7fd3bac
gretea5 <gretea4489@gmail.com>
2023-03-04 20:44:25 +0900

    feat : 채팅 목록 틀 생성

--- src/pages/mypage/ChattingListPage.js
+++ src/pages/mypage/ChattingListPage.js
@@ -3,20 +3,36 @@ import styled from "styled-components";
 
 const ChattingListDiv = styled.div`
     width: 490px;
-    max-height: 510px;
+    height: 510px;
     border-style: solid;
     border-color: green;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     overflow-x: hidden;
-    overflow-y: scroll;
+    overflow-y: auto;
+`;
+
+const ChattingElement = styled.div`
+    width: 470px;
+    min-height: 100px;
+    border-style: solid;
+    border-color: red; 
 `;
 
 const ChattingListPage = () => {
     return (
     <>
-        <ChattingListDiv>
+        <ChattingListDiv>    
+            <ChattingElement/>
+            <ChattingElement/>
+            <ChattingElement/>
+            <ChattingElement/>
+            <ChattingElement/>
+            <ChattingElement/>
+            <ChattingElement/>
+            <ChattingElement/>
+            <ChattingElement/>
         </ChattingListDiv>
     </>
     );
