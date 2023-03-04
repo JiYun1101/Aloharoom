c9cd4b5606029833382eeff2911a6e141a9409ea
gretea5 <gretea4489@gmail.com>
2023-03-04 18:25:45 +0900

    feat : ChattingListDiv 생성

--- src/pages/mypage/ChattingListPage.js
+++ src/pages/mypage/ChattingListPage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const ChattingListDiv = styled.div`
+    width: 490px;
+    max-height: 510px;
+    border-style: solid;
+    border-color: green;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    overflow-x: hidden;
+    overflow-y: scroll;
+`;
 
 const ChattingListPage = () => {
-    return (<>
-        <h1>ChattingListPage</h1>
-    </>);
+    return (
+    <>
+        <ChattingListDiv>
+        </ChattingListDiv>
+    </>
+    );
 }
 
 export default ChattingListPage;
