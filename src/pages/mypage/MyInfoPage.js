bd88f554c7da53b60ba322cf4606d955d4cddb5a
gretea5 <gretea4489@gmail.com>
2023-03-05 23:16:46 +0900

    feat : MyInfoDiv 틀 생성

--- src/pages/mypage/MyInfoPage.js
+++ src/pages/mypage/MyInfoPage.js
@@ -1,9 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const MyInfoDiv = styled.div`
+    margin-top: 1.5rem;
+    margin-left: 0.5rem;
+    width: 480px;
+    height: 480px;
+    border-style: solid;
+    border-color: red;
+    display: flex;
+    flex-direction: column;
+`;
 
 const MyInfoPage = () => {
     return (
         <>
-            <h1>MyInfoPage</h1>
+            <MyInfoDiv>
+            </MyInfoDiv>
         </>
     );
 }
