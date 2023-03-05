3cfe4f8e34a7c8725895a75e5eaa78abc1290bd6
gretea5 <gretea4489@gmail.com>
2023-03-05 15:26:12 +0900

    feat : 좋아요 목록 틀 생성

--- src/pages/mypage/LikedListPage.js
+++ src/pages/mypage/LikedListPage.js
@@ -2,10 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const LikedListDiv = styled.div`
-    margin-top: 0.5rem;
+    margin-top: 1.5rem;
     margin-left: 1.2rem;
     width: 480px;
-    max-height: 500px;
+    max-height: 480px;
     display: flex;  
     flex-direction: row;
     flex-wrap: wrap;
@@ -29,29 +29,71 @@ const LikedListDiv = styled.div`
 `;
 
 const LikedElementDiv = styled.div`
+    position: relative;
     width: 110px;
     height: 150px;
     border-style: solid;
     border-color: blue;
 `;
 
+const LikedPostCard = styled.div`
+    position: absolute;
+    width: 105px;
+    height: 145px;
+`;
+
 const LikedListPage = () => {
     return (
     <>
         <LikedListDiv>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
-            <LikedElementDiv/>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
+            <LikedElementDiv>
+                <LikedPostCard/>
+            </LikedElementDiv>
         </LikedListDiv>
     </>
     );
