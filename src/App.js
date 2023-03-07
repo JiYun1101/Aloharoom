ddc01bcb2d4bf6fb641a3e3e66b08bdfa3e7dd14
JIYun1101 <gina7440@naver.com>
2023-03-07 18:33:29 +0900

    fit_App.js에 RegisterPage2추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import RegisterPage from "./pages/RegisterPage";
 import TermUser from "./pages/TermUser";
 import MyPage from "./pages/mypage/MyPage";
+import RegisterPage2 from "./pages/RegisterPage2";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/postMapPage" element={<PostMapPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registerPage" element={<RegisterPage />} />
+        <Route path="RegisterPage/registerPage2" element={<RegisterPage2 />} />
         <Route path="/termUser" element={<TermUser />} />
         <Route path="/myPage" element={<MyPage />} />
         <Route path="*" element={<>Not found</>} />
