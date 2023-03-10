18bfdc3260c91fd9293363d2d9696f87c2252106
gretea5 <gretea4489@gmail.com>
2023-03-10 14:57:15 +0900

    build : 분리할 컴포넌트 생성

---
+++ src/pages/postmappage/MapPostComponents/CardPost.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardPost = () => {
+    return (<></>);
+}
+
+export default CardPost;
---
+++ src/pages/postmappage/MapPostComponents/CardPosts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardPosts = () => {
+    return (<></>);
+}
+
+export default CardPosts;
