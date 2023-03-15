5adc1acd77ad13f053aa6efb8df2a600ee23337d
gretea5 <gretea4489@gmail.com>
2023-03-15 17:06:40 +0900

    feat : NotificationTitle, closeButton 위치 설정

--- src/pages/NotificationModal.js
+++ src/pages/NotificationModal.js
@@ -15,13 +15,34 @@ const NotificationModalDiv = styled.div`
     border-radius: 0.5rem;
 `;
 
-const NoticationModalCloseButtonDiv = styled.div`
+const NotificationModalHeaderDiv = styled.div`
+    width: 20rem;
+    height: 3rem;
+    display: flex;
+    flex-direction: row;
+`
+
+const NotificationTitleDiv = styled.div`
+    width: 10rem;
+    height: 3rem;
+`;
+
+const NotificationTitle = styled.div`
+    margin-top: 0.7rem;
+    margin-left: 0.5rem;
+    font-weight: 400;
+    font-size: 1.5rem;
+`;
+
+const NotificationModalCloseButtonDiv = styled.div`
+    width: 10rem;
+    height: 2rem;
     display: flex;
     flex-direction: row-reverse;
 `;
 
 const NotificationModalCloseButton = styled.button`
-    margin-right: 0.2rem;
+    margin-right: 0.5rem;
     font-size: 1.5rem;
     color: #47a5fd;
     background-color: white;
@@ -32,9 +53,14 @@ const NotificationModalCloseButton = styled.button`
 const NotificationModal = ({ModalClose = f => f}) => {
     return (
         <NotificationModalDiv>
-            <NoticationModalCloseButtonDiv>
-                <NotificationModalCloseButton onClick={ModalClose}>x</NotificationModalCloseButton>
-            </NoticationModalCloseButtonDiv>
+            <NotificationModalHeaderDiv>
+                <NotificationTitleDiv>
+                    <NotificationTitle>알림</NotificationTitle>
+                </NotificationTitleDiv>
+                <NotificationModalCloseButtonDiv>
+                    <NotificationModalCloseButton onClick={ModalClose}>x</NotificationModalCloseButton> 
+                </NotificationModalCloseButtonDiv>
+            </NotificationModalHeaderDiv>
         </NotificationModalDiv>
     );
 }
