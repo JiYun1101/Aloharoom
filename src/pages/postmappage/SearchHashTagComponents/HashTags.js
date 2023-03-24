f354cd377ac643b568a7c43ca75a73dcd00340db
gretea5 <gretea4489@gmail.com>
2023-03-25 00:01:07 +0900

    chore : 해시태그 버튼형식으로 변경
    차후 slider 사용 예정

--- src/pages/postmappage/SearchHashTagComponents/HashTags.js
+++ src/pages/postmappage/SearchHashTagComponents/HashTags.js
@@ -14,17 +14,28 @@ const TagTotalStyle = {
     fontWeight : "800"
 }
 
+const HashTagButton = styled.button`
+    background-color: white;
+    width: 7.5rem;
+    height: 2rem;
+    border: 2px solid #bbbbbb;
+    border-radius: 1rem;
+    color: #bbbbbb;
+    font-weight: 500;
+    font-size: 1.2rem;
+`;
+
 const HashTags = () => {
     return (
         <HashSection>
             <HStack
-                spacing={25} 
+                spacing={10} 
                 style={TagTotalStyle}>
-                <Tag>#해시태그1</Tag>
-                <Tag>#해시태그2</Tag>
-                <Tag>#해시태그3</Tag>
-                <Tag>#해시태그4</Tag>
-                <Tag>#해시태그5</Tag>
+                <HashTagButton>#주차가능</HashTagButton>
+                <HashTagButton>#주변 편의점</HashTagButton>
+                <HashTagButton>#공원</HashTagButton>
+                <HashTagButton>#층간소음</HashTagButton>
+                <HashTagButton>#남향</HashTagButton>
             </HStack>
         </HashSection>
     );
