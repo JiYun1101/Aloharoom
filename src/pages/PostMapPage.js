92daeca1d42c290faa2b9910d6d0542b1cf5f2e7
gretea5 <gretea4489@gmail.com>
2023-03-27 17:00:36 +0900

    feat : axios 전체 게시물 데이터 받아오기

--- src/pages/PostMapPage.js
+++ src/pages/PostMapPage.js
@@ -1,10 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import Header from "./Header";
 import PostMapContent from "./postmappage/PostMapContent";
 
-
-// import { useEffect } from "react";
-// import axios from 'axios';
 
 const PostMapPage = () => {
     // async function getData() {
@@ -19,6 +17,23 @@ const PostMapPage = () => {
     // useEffect(() => {
     //     getData();
     // }, []);
+    
+    //전체 게시물 데이터 받아오기
+    async function fetchViewPostData() {
+        await axios.get('http://localhost:8080/api/board')
+            .then((response) => {
+                console.log('response.data : ', response.data);
+            })
+            .catch((error) => {
+                console.log('axios error');
+            })
+    }
+
+    //한번 렌더링 될때 데이터를 받아온다.
+    useEffect(() => {
+        fetchViewPostData();
+    }, []);
+
     return (
     <>
         <Header/>
