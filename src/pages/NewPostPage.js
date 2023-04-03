db326de72a411cd08951bc3adcefaa1fee7a8c2e
gretea5 <gretea4489@gmail.com>
2023-04-03 22:36:03 +0900

    feat: 새 글 쓰기 페이지 상단 제목, 이전페이지 돌아가는 부분 구현

--- src/pages/NewPostPage.js
+++ src/pages/NewPostPage.js
@@ -1,7 +1,43 @@
 import React from "react";
+import styled from "styled-components";
+import { AiOutlineLeft } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+
+const NewPostContainer = styled.div`
+    position: relative;
+    //border-style: solid;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+const NewPostHeaderDiv = styled.div`
+    margin-top: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    font-weight: 500;
+    color: #47a5fd;
+`;
+
+const BackPageIconStyle = {
+    position: "absolute",
+    top: "1rem",
+    left: "1rem",
+    color: "#47a5fd"
+}
 
 const NewPostPage = () => {
-    return (<h1>새 글 쓰기 페이지</h1>);
+    const navigate = useNavigate();
+    return (
+        <NewPostContainer>
+            <AiOutlineLeft size={40} style={BackPageIconStyle} onClick={() => navigate(-1)}/>
+            <NewPostHeaderDiv>
+                새 글 쓰기
+            </NewPostHeaderDiv>
+        </NewPostContainer>
+    );
 }
 
 export default NewPostPage;
