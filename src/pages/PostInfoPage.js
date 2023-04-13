b7a5da2c6d3c326c55edf35d802985ca25935a6a
gretea5 <gretea4489@gmail.com>
2023-04-13 21:55:46 +0900

    feat: 게시물 상세보기 페이지 틀 생성

--- src/pages/PostInfoPage.js
+++ src/pages/PostInfoPage.js
@@ -1,7 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+
+const PostInfoPageContainer = styled.div`
+    height: 80rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const PostInfoPageBox = styled.div`
+    border-style: solid;
+    width: 50rem;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+`;
+
+// const PostInfoImageBox = styled.div`
+//     margin-top: 3rem;
+//     border-style: solid;
+//     width: 100%;
+//     height: 15rem;
+// `;
 
 const PostInfoPage = () => {
-    return (<></>);
+    return (
+        <PostInfoPageContainer>
+            <PostInfoPageBox>
+                {/* <PostInfoImageBox>
+
+                </PostInfoImageBox> */}
+            </PostInfoPageBox>
+        </PostInfoPageContainer>
+    );
 }
 
 export default PostInfoPage;
