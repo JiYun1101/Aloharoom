9850a55341e60dae6dcee5b5c7cc574fdd713280
gretea5 <gretea4489@gmail.com>
2023-04-16 11:20:44 +0900

    refactor: 새 글 쓰기 페이지 컴포넌트 헤더 내용 영역 분리

---
+++ src/pages/newpostpage/NewPostHeaderSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const NewPostHeaderDiv = styled.div`
+    height: 3rem;
+    margin-top: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    font-weight: 500;
+    color: #47a5fd;
+`;
+
+const NewPostHeaderSection = () => {
+    return(
+        <NewPostHeaderDiv>새 글 쓰기</NewPostHeaderDiv>
+    );
+}
+
+export default NewPostHeaderSection;
