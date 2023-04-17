65cb9f6e266dfdbfde729c98a5aafb3487eaf963
JIYun1101 <gina7440@naver.com>
2023-04-18 02:28:06 +0900

    fix: 오류 수정

--- src/pages/postmappage/SearchHashTag.js
+++ src/pages/postmappage/SearchHashTag.js
@@ -12,13 +12,13 @@ const SearchHashTagContainer = styled.div`
   flex-direction: column;
 `;
 
-const SearchHashTag = ({setSearchStr}) => {
-    return (
-        <SearchHashTagContainer>
-            <Search setSearchStr={setSearchStr}/>
-            <HashTagTitle/>
-        </SearchHashTagContainer>
-    );
-}
+const SearchHashTag = ({ setSearchStr }) => {
+  return (
+    <SearchHashTagContainer>
+      <Search setSearchStr={setSearchStr} />
+      <HashTagTitle />
+    </SearchHashTagContainer>
+  );
+};
 
 export default SearchHashTag;
