4ac66d44e18a2e3248a08e1c3a7303449c97a171
gretea5 <gretea4489@gmail.com>
2023-04-19 23:25:08 +0900

    chore: 전체 게시물 데이터 받아오는 함수 위치 조정

--- src/pages/postmappage/PostMapContent.js
+++ src/pages/postmappage/PostMapContent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchHashTag from "./SearchHashTag";
 
 import styled from "styled-components";
@@ -6,19 +6,20 @@ import MapPost from "./MapPost";
 
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const PostMapContentContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 50rem;
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
+    position: relative;
+    width: 100%;
+    height: 50rem;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
 `;
 
 const LinkToStyle = {
-  textDecoration: "none",
-  color: "inherit",
+    textDecoration: "none",
+    color: "inherit",
 };
 
 const NewPostIconStyle = {
@@ -30,6 +31,23 @@ const NewPostIconStyle = {
 };
 
 const PostMapContent = () => {
+    //전체 게시물 데이터 받아오기
+    async function fetchViewPostData() {
+        await axios
+        .get("http://localhost:8080/api/board")
+        .then((response) => {
+            console.log("ViewPostData => response.data : ", response.data);
+        })
+        .catch((error) => {
+            console.log("axios error");
+        });
+    }
+
+    //한번 렌더링 될때 데이터를 받아온다.
+    useEffect(() => {
+        fetchViewPostData();
+    }, []);
+
     //여기에 검색에 대한 상태 변수를 지정
     const [searchStr, setSearchStr] = useState(null);
     return (
