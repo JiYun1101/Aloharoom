f3ce037faa6f02125529403928f5a5eb187c0088
JIYun1101 <gina7440@naver.com>
2023-05-05 06:41:37 +0900

    feat: map graph 연동(미완)

--- src/pages/About/Graph_map.js
+++ src/pages/About/Graph_map.js
@@ -2,56 +2,89 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { Scatter } from "@ant-design/plots";
 import map_korea from "../../img/map_korea.jpg";
+import axios from "axios";
 
 const DemoScatter = () => {
-  const data = [
-    {
-      city: "경상도",
-      搜索UV: 1.5,
-      端DAU: 6,
-      搜索DAU渗透率: 3,
-    },
-    {
-      city: "전라도",
-      搜索UV: 2,
-      端DAU: 5,
-      搜索DAU渗透率: 13,
-    },
-    {
-      city: "경기도",
-      搜索UV: 7,
-      端DAU: 3.6,
-      搜索DAU渗透率: 16,
-    },
-    {
-      city: "서울",
-      搜索UV: 5,
-      端DAU: 5,
-      搜索DAU渗透率: 16,
-    },
-    {
-      city: "강원도",
-      搜索UV: 2,
-      端DAU: 1,
-      搜索DAU渗透率: 19,
-    },
-    {
-      city: "충청도",
-      搜索UV: 7,
-      端DAU: 2,
-      搜索DAU渗透率: 90,
-    },
-    {
-      city: "제주도",
-      搜索UV: 7.4,
-      端DAU: 1.5,
-      搜索DAU渗透率: 30,
-    },
-  ];
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const {
+        data: { userTotal, regionBoardMap },
+      } = await axios.get("http://localhost:8080/api/data");
+
+      const modifiedData = [
+        {
+          city: "울산",
+          搜索UV: 3,
+          端DAU: 1.8,
+          搜索DAU渗透率: 3,
+        },
+        {
+          city: "광주",
+          搜索UV: 1.8,
+          端DAU: 1.8,
+          搜索DAU渗透率: 13,
+        },
+        {
+          city: "경기도",
+          搜索UV: 1.88,
+          端DAU: 4.2,
+          搜索DAU渗透率: 16,
+        },
+        {
+          city: "서울",
+          搜索UV: 1.8,
+          端DAU: 4,
+          搜索DAU渗透率: 16,
+        },
+        {
+          city: "강원도",
+          搜索UV: 2.8,
+          端DAU: 4,
+          搜索DAU渗透率: 19,
+        },
+        {
+          city: "대전",
+          搜索UV: 2.1,
+          端DAU: 3,
+          搜索DAU渗透率: 90,
+        },
+        {
+          city: "제주도",
+          搜索UV: 1.6,
+          端DAU: 0.0,
+          搜索DAU渗透率: 30,
+        },
+      ];
+
+      const { data: newData } = await axios.get(
+        "http://localhost:8080/api/data"
+      );
+      modifiedData.forEach((item) => {
+        const target = newData.find((city) => city.city === item.city);
+        if (target) {
+          item.搜索DAU渗透率 = target.搜索DAU渗透率;
+        }
+      });
+
+      setData(modifiedData);
+
+      console.log("Data loaded successfully");
+    } catch (error) {
+      console.error("Data loading failed:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const config = {
-    width: 800,
-    height: 400,
-    autoFit: false,
+    width: 720,
+    height: 663,
+    container: "chart-container",
+    autoFit: true,
     appendPadding: 16,
     data,
     xField: "搜索UV",
@@ -75,6 +108,7 @@ const DemoScatter = () => {
           端DAU: (value) => value + "万",
           搜索DAU渗透率: () => "%",
         };
+
         let htmlStr = `<div style="margin:10px 0;font-weight:700;">${field?.data?.city}</div><div class="g2-tooltip-items">`;
         items.forEach((item) => {
           htmlStr += `<div class="g2-tooltip-item" style="margin-bottom:8px;display:flex;justify-content:space-between;">
@@ -82,7 +116,9 @@ const DemoScatter = () => {
                   item.name
                 }</span>
                 <span class="g2-tooltip-item-value">${
-                  item.value + formatterInfo[item.name](item.value)
+                  item.name === "搜索DAU渗透率"
+                    ? field.data[item.name] + "%"
+                    : item.value + formatterInfo[item.name](item.value)
                 }</span>
               </div>`;
         });
@@ -91,6 +127,8 @@ const DemoScatter = () => {
       },
     },
     xAxis: {
+      min: 0,
+      max: 5,
       grid: {
         line: {
           style: {
@@ -98,139 +136,18 @@ const DemoScatter = () => {
           },
         },
       },
-      label: {
-        formatter: (v) => (v !== "0" ? v + "%" : v),
+      yAxis: {
+        min: 0,
+        max: 5, // 최대값을 5로 지정
+        line: null,
+        label: {
+          formatter: (v) => (v !== "0" ? v + "%" : v),
+        },
       },
       line: null,
-    },
-    label: {
-      formatter: (item) => {
-        return item.city;
-      },
-      offsetY: 12,
-      style: {
-        fontSize: 12,
-        fill: "rgba(0,0,0,0.85)",
-      },
-    },
-    yAxis: {
-      min: 0,
-      line: null,
-      label: {
-        formatter: (v) => (v !== "0" ? v + "%" : v),
-      },
-    },
-    annotations: [
-      {
-        type: "text",
-        position: [4, 8],
-        content: "搜索DAU渗透率",
-        offsetY: -8,
-        style: {
-          fontSize: 12,
-          textAlign: "center",
-        },
-      },
-      {
-        type: "text",
-        position: [8, 4],
-        content: "搜索DAU渗透率",
-        rotate: Math.PI / 2,
-        offsetY: -40,
-        offsetX: 8,
-        style: {
-          fontSize: 12,
-        },
-      },
-      {
-        type: "region",
-        start: [7, 7],
-        end: [7.8, 7.8],
-        top: true,
-        style: {
-          fill: "#fff",
-          fillOpacity: 0.5,
-          opacity: 1,
-        },
-      },
-      {
-        type: "region",
-        start: [0.2, 7],
-        end: [1, 7.8],
-        top: true,
-        style: {
-          fill: "#fff",
-          fillOpacity: 0.5,
-          opacity: 1,
-        },
-      },
-      {
-        type: "region",
-        start: [7, 0.2],
-        end: [7.8, 1],
-        top: true,
-        style: {
-          fill: "#fff",
-          fillOpacity: 0.5,
-          opacity: 1,
-        },
-      },
-    ],
-    quadrant: {
-      xBaseline: 4,
-      yBaseline: 4,
-      lineStyle: {
-        lineDash: [4, 2],
-        lineWidth: 2,
-      },
-      regionStyle: [
-        // {
-        //   fill: "#5bd8a6",
-        //   fillOpacity: 0.1,
-        // },
-        // {
-        //   fill: "#667796",
-        //   fillOpacity: 0.1,
-        // },
-        // {
-        //   fill: "#fff",
-        // },
-        // {
-        //   fill: "#f7664e",
-        //   fillOpacity: 0.1,
-        // },
-      ],
-      labels: [
-        {
-          content: "热门市场",
-          position: [7.2, 7],
-          style: {
-            fill: "rgba(0,0,0, 0.85)",
-            textAlign: "start",
-          },
-        },
-        {
-          content: "潜力市场",
-          position: [0.2, 7],
-          style: {
-            fill: "rgba(0,0,0, 0.85)",
-            textAlign: "start",
-          },
-        },
-        {
-          content: "",
-        },
-        {
-          content: "提频市场",
-          position: [7.2, 1],
-          style: {
-            fill: "rgba(0,0,0, 0.85)",
-            textAlign: "start",
-          },
-        },
-      ],
     },
   };
+
   return (
     <div style={{ position: "relative" }}>
       <img src={map_korea} alt="map" />
