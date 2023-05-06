b010bb08da71ca0c78baae4029351a0f38aceb46
gretea5 <gretea4489@gmail.com>
2023-05-06 19:42:43 +0900

    refactor: 알림 모달 코드 리팩토링
    필요없는 컴포넌트 제거

--- src/pages/NotificationModal.js
+++ src/pages/NotificationModal.js
@@ -1,44 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
-const NotificationModalDiv = styled.div`
+const NotificationModalContainer = styled.div`
     position: absolute;
     z-index: 5;
     width: 20rem;
     height: 20rem;
     background-color: white;
     right: 14vw;
-
     border-style: solid;
     border-width: 0.1rem;
     border-color: #47a5fd;
     border-radius: 0.5rem;
 `;
 
-const NotificationModalHeaderDiv = styled.div`
-    width: 20rem;
-    height: 3rem;
+const NotificationModalFlexDiv = styled.div`
+    width: ${props => props.width || "0rem"};
+    height: ${props => props.height || "0rem"};
     display: flex;
-    flex-direction: row;
-`
-
-const NotificationTitleDiv = styled.div`
-    width: 10rem;
-    height: 3rem;
+    flex-direction:${props => props.flexDirection || "row"};
 `;
 
-const NotificationTitle = styled.div`
-    margin-top: 0.7rem;
-    margin-left: 0.5rem;
-    font-weight: 400;
-    font-size: 1.5rem;
-`;
-
-const NotificationModalCloseButtonDiv = styled.div`
-    width: 10rem;
-    height: 2rem;
-    display: flex;
-    flex-direction: row-reverse;
+const NotificationModalDiv = styled.div`
+    width: ${props => props.width};
+    height: ${props => props.height};
+    font-weight: ${props => props.fontWeight};
+    font-size: ${props => props.fontSize};
+    margin-top: ${props => props.marginTop};
+    margin-left: ${props => props.marginLeft};
 `;
 
 const NotificationModalCloseButton = styled.button`
@@ -50,18 +39,18 @@ const NotificationModalCloseButton = styled.button`
     border-style:none;
 `;
 
-const NotificationModal = ({ModalClose = f => f}) => {
+const NotificationModal = ({ModalClose}) => {
     return (
-        <NotificationModalDiv>
-            <NotificationModalHeaderDiv>
-                <NotificationTitleDiv>
-                    <NotificationTitle>알림</NotificationTitle>
-                </NotificationTitleDiv>
-                <NotificationModalCloseButtonDiv>
+        <NotificationModalContainer>
+            <NotificationModalFlexDiv width="20rem" height="3rem">
+                <NotificationModalDiv width="10rem" height="3rem">
+                    <NotificationModalDiv fontWeight="400" fontSize="1.5rem" marginTop="0.7rem" marginLeft="0.5rem">알림</NotificationModalDiv>
+                </NotificationModalDiv>
+                <NotificationModalFlexDiv width="10rem" height="2rem" flexDirection="row-reverse">
                     <NotificationModalCloseButton onClick={ModalClose}>x</NotificationModalCloseButton> 
-                </NotificationModalCloseButtonDiv>
-            </NotificationModalHeaderDiv>
-        </NotificationModalDiv>
+                </NotificationModalFlexDiv>
+            </NotificationModalFlexDiv>
+        </NotificationModalContainer>
     );
 }
 
