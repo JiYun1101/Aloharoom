a906e6e0560a8467f911d1144fc93a187167e81a
JIYun1101 <gina7440@naver.com>
2023-05-06 20:15:40 +0900

    feat: userTotal 그래프 수정

--- src/pages/About/Graph_number.js
+++ src/pages/About/Graph_number.js
@@ -1,27 +1,68 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Button, Col, Row, Statistic } from "antd";
 
-const App = () => (
-  <Row justify="center" gutter={16}>
-    <Col span={12}>
-      <div
-        style={{
-          border: "1px solid #000",
-          borderRadius: 8, // 둥근 모서리 적용
-          padding: 16,
-        }}
-      >
-        <Statistic title="이용자 수" value={112893} precision={2} />
-        <Button
-          style={{
-            marginTop: 16,
-          }}
-          type="primary"
-        >
-          Recharge
-        </Button>
-      </div>
-    </Col>
-  </Row>
-);
+const Graph_number = () => {
+  const [userTotal, setUserTotal] = useState(0);
+  const [isFetching, setIsFetching] = useState(false);
 
-export default App;
+  const fetchData = () => {
+    setIsFetching(true);
+    axios
+      .get("http://localhost:8080/api/data")
+      .then((response) => {
+        const newData = response.data;
+        const parsedTotal = parseFloat(newData.userTotal);
+        let count = 0;
+        const intervalId = setInterval(() => {
+          if (count === parsedTotal) {
+            setIsFetching(false);
+            clearInterval(intervalId);
+          } else {
+            count++;
+            setUserTotal(count);
+          }
+        }, 1000 / parsedTotal);
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsFetching(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleRecharge = () => {
+    fetchData();
+  };
+
+  return (
+    <Row justify="center" gutter={16}>
+      <Col span={12}>
+        <div
+          style={{
+            border: "1px solid #000",
+            borderRadius: 8,
+            padding: 16,
+          }}
+        >
+          <Statistic title="이용자 수" value={userTotal} precision={2} />
+          <Button
+            style={{
+              marginTop: 16,
+            }}
+            type="primary"
+            onClick={handleRecharge}
+            loading={isFetching}
+          >
+            Recharge
+          </Button>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default Graph_number;
