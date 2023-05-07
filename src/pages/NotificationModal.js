6b7d6b33728899a09293508a8393b90e87b9faa4
gretea5 <gretea4489@gmail.com>
2023-05-07 22:57:15 +0900

    feat: 알림 모달 데이터 받아와서 뿌려주기

--- src/pages/NotificationModal.js
+++ src/pages/NotificationModal.js
@@ -1,4 +1,6 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const NotificationModalContainer = styled.div`
@@ -19,6 +21,23 @@ const NotificationModalFlexDiv = styled.div`
     height: ${props => props.height || "0rem"};
     display: flex;
     flex-direction:${props => props.flexDirection || "row"};
+    align-items:${props => props.alignItems};
+    margin-top: ${props => props.marginTop};
+    margin-left: ${props => props.marginLeft};
+    overflow-y: ${props => props.overFlowY};
+    border-bottom: ${props => props.borderBottom};
+    &::-webkit-scrollbar {
+        width: 0.5rem;          /* 스크롤바의 너비 */
+    }
+
+    &::-webkit-scrollbar-thumb {
+        height: 1%;             /* 스크롤바의 길이 */
+        background: #bbbbbb;    /* 스크롤바의 색상 */
+        border-radius: 1rem;
+    }
+    &::-webkit-scrollbar-track {
+        background: white;      /*스크롤바 뒷 배경 색상*/
+    }
 `;
 
 const NotificationModalDiv = styled.div`
@@ -30,6 +49,13 @@ const NotificationModalDiv = styled.div`
     margin-left: ${props => props.marginLeft};
 `;
 
+const NotificationModalSpan = styled.span`
+    color: #47a5fd;
+    font-size: ${props => props.fontSize};
+    font-weight: ${props => props.fontWeight};
+    margin-right: ${props => props.marginRight};
+`;
+
 const NotificationModalCloseButton = styled.button`
     margin-right: 0.5rem;
     font-size: 1.5rem;
@@ -39,7 +65,26 @@ const NotificationModalCloseButton = styled.button`
     border-style:none;
 `;
 
+const LinkToStyle = {
+    textDecoration: "none",
+    color: "inherit",
+};
+
 const NotificationModal = ({ModalClose}) => {
+    const [notificationData, setNotificationData] = useState([]);
+    async function fetchNotificationInfo() {
+        await axios.get(`http://localhost:8080/api/notification`, {
+            withCredentials:true
+        })
+        .then((response) => {
+            console.log('notification Data:', response.data);
+            setNotificationData(response.data);
+        })
+        .catch((error) => { console.log(`fetchNotificationInfo axios error`);})
+    }
+    useEffect(() => {
+        fetchNotificationInfo();
+    }, []);
     return (
         <NotificationModalContainer>
             <NotificationModalFlexDiv width="20rem" height="3rem">
@@ -49,6 +94,28 @@ const NotificationModal = ({ModalClose}) => {
                 <NotificationModalFlexDiv width="10rem" height="2rem" flexDirection="row-reverse">
                     <NotificationModalCloseButton onClick={ModalClose}>x</NotificationModalCloseButton> 
                 </NotificationModalFlexDiv>
+            </NotificationModalFlexDiv>
+            <NotificationModalFlexDiv width="99%" height="16.5rem" flexDirection="column" overFlowY="auto">
+                {notificationData.map((data, index) => (
+                    <Link to={data.flag === 0 ? `./postInfoPage/${data.boardId}`: ``} style={LinkToStyle}>
+                        <NotificationModalFlexDiv 
+                            key={index} 
+                            width="98%" 
+                            height="5rem" 
+                            flexDirection="column" 
+                            borderBottom="1px solid #47a5fd"
+                        >
+                            <NotificationModalDiv width="100%" height="60%">
+                                <NotificationModalSpan>
+                                    {data.flag === 0 ? `방 보기 페이지 ${data.content}` : `커뮤니티 페이지 ${data.content}`}
+                                </NotificationModalSpan>
+                            </NotificationModalDiv>
+                            <NotificationModalFlexDiv width="100%" height="40%" flexDirection="row-reverse" alignItems="center">
+                                <NotificationModalSpan marginRight="0.5rem">{data.createdDate}</NotificationModalSpan>
+                            </NotificationModalFlexDiv>
+                        </NotificationModalFlexDiv>
+                    </Link>
+                ))}
             </NotificationModalFlexDiv>
         </NotificationModalContainer>
     );
