e48004c4469e93ab6f20e50f771b565e0cf9250a
gretea5 <gretea4489@gmail.com>
2023-05-11 13:56:51 +0900

    feat: 커뮤니티 글 검색 기능

--- src/pages/Community/Post.js
+++ src/pages/Community/Post.js
@@ -3,8 +3,10 @@ import styled from "styled-components";
 import CardPosts from "./CardPosts";
 import PostNavs from "./PostNavs";
 import { UserOutlined } from "@ant-design/icons";
-import { Avatar, Segmented, Space } from "antd";
+import { Avatar, Input, Segmented, Select, Space } from "antd";
 import { Pagination } from "antd";
+import axios from "axios";
+import baseURL from "../api/baseURL";
 // import CardPost2 from "./Community";
 
 const Page = styled.div`
@@ -47,11 +49,52 @@ const CardPost3 = styled.div`
 const Post = ({ clickedCommunityId, cardPostData }) => {
   const [roommatePosts, setRoomMatePosts] = useState(true);
   const [sharehousePosts, setShareHousePosts] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [code, setCode] = useState(1);
+  const [selectSearchType, setSelectSearchType] = useState('제목');
+  const [searchCardData, setSearchCardData] = useState([]);
+
+  const SearchTypeChange = (value) => {
+    setSelectSearchType(value);
+  }
 
   const roommatePostsClick = () => {
     setRoomMatePosts(true);
     setShareHousePosts(false);
   };
+
+  const handleInputChange = (e) => {
+    console.log('value', e.target.value);
+    setInputValue(e.target.value);
+  }
+
+  async function CommunityPostTitleSearch(keyword, code) {
+    await axios.get(`${baseURL}/api/communitySearch`, {
+      params: {
+        keyword: keyword,
+        code: code
+      }
+    })
+    .then((response) => {
+      console.log('response.data', response.data);
+      setSearchCardData(response.data);
+    })
+    .catch((error) => { console.log(`CommunityPostTitleSearch axios error`);})
+  }
+
+  async function CommunityPostNicknameSearch(nickname, code) {
+    await axios.get(`${baseURL}/api/communitySearch/nickname`, {
+      params: {
+        nickname: nickname,
+        code: code
+      }
+    })
+    .then((response) => {
+      console.log('response.data', response.data);
+      setSearchCardData(response.data);
+    })
+    .catch((error) => { console.log(`CommunityPostNicknameSearch axios error`);})
+  }
 
   return (
     <>
@@ -66,6 +109,7 @@ const Post = ({ clickedCommunityId, cardPostData }) => {
                       style={{
                         padding: 4,
                       }}
+                      onClick={() => { setCode('1');}}
                     >
                       <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
                       <div>User 1</div>
@@ -79,6 +123,7 @@ const Post = ({ clickedCommunityId, cardPostData }) => {
                       style={{
                         padding: 4,
                       }}
+                      onClick={() => { setCode('2');}}
                     >
                       <Avatar
                         style={{
@@ -98,6 +143,7 @@ const Post = ({ clickedCommunityId, cardPostData }) => {
                       style={{
                         padding: 4,
                       }}
+                      onClick={() => { setCode('3');}}
                     >
                       <Avatar
                         style={{
@@ -127,6 +173,40 @@ const Post = ({ clickedCommunityId, cardPostData }) => {
           <p>3. 오늘의 일기</p>
         </CardPost3>{" "}
         {/* CardPost3에 ref 추가 */}
+        <Input 
+          placeholder={selectSearchType === '제목'? `제목을 입력하세요.` : `닉네임을 입력하세요.`}
+          size="small"
+          onChange={handleInputChange}
+        />
+        <Select 
+          defaultValue="제목"
+          style={{
+            width: 120,
+          }}
+          options={[
+          {
+            value: '제목',
+            label: '제목'
+          },
+          {
+            value: '닉네임',
+            label: '닉네임'
+          },
+        ]}
+          onChange={SearchTypeChange}
+        />
+        <button
+          onClick={() => {
+            if (selectSearchType === '제목') {
+              CommunityPostTitleSearch(inputValue, code);
+            }
+            else {
+              CommunityPostNicknameSearch(inputValue, code);
+            }
+          }}
+        >
+          검색
+        </button>
         <PageNum>
           <Pagination defaultCurrent={1} total={50} />
         </PageNum>
