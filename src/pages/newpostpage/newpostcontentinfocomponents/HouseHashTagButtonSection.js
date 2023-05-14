d2a94d87861ec4115c7e7bfa77ac9d6f6a42b972
gretea5 <gretea4489@gmail.com>
2023-05-15 01:53:24 +0900

    chore: HoverHashtagButton으로 변경

--- src/pages/newpostpage/newpostcontentinfocomponents/HouseHashTagButtonSection.js
+++ src/pages/newpostpage/newpostcontentinfocomponents/HouseHashTagButtonSection.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import styled from 'styled-components';
+import HoverHashTagButton from '../../HoverHashTagButton';
 
 const HouseHashTagButtonDiv = styled.div`
   width: 90%;
@@ -11,24 +12,7 @@ const HouseHashTagButtonDiv = styled.div`
   gap: 0.5rem;
 `;
 
-const HouseHashTagButton = styled.button`
-  width: 7rem;
-  height: 2rem;
-  font-size: 1rem;
-  border-width: 0.1rem;
-  border-style: solid;
-  border-radius: 0.3rem;
-  background-color: white;
-  border-color: ${(props) => (props.selected ? "#47A5FD" : "#bbbbbb")};
-  color: ${(props) => (props.selected ? "#47A5FD" : "#bbbbbb")};
-  :hover {
-    border-color: #47a5fd;
-    color: #47a5fd;
-  }
-`;
-
 const HouseHashTagButtonSection = ({ myHomeHashtags }) => {
-    //집 해시태그 버튼 클릭 상태 변수
     const [selectedHouseHashTagButtons, setSelectedHouseHashTagButtons] = useState([]);
     const handleHouseHashTagButtonClick = (index) => {
         if (selectedHouseHashTagButtons.includes(index)) {
@@ -42,13 +26,13 @@ const HouseHashTagButtonSection = ({ myHomeHashtags }) => {
     return (
         <HouseHashTagButtonDiv>
           {myHomeHashtags.map((data, idx) => (
-            <HouseHashTagButton
+            <HoverHashTagButton
               key={idx}
               selected={selectedHouseHashTagButtons.includes(data)}
               onClick={() => handleHouseHashTagButtonClick(data)}
             >
               {data}
-            </HouseHashTagButton>
+            </HoverHashTagButton>
           ))}
       </HouseHashTagButtonDiv>
     );
--- src/pages/newpostpage/newpostcontentinfocomponents/MyHashTagButtonSection.js
+++ src/pages/newpostpage/newpostcontentinfocomponents/MyHashTagButtonSection.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import styled from 'styled-components';
+import HoverHashTagButton from '../../HoverHashTagButton';
 
 const MyHashTagButtonDiv = styled.div`
     width: 90%;
@@ -11,24 +12,7 @@ const MyHashTagButtonDiv = styled.div`
     gap: 0.5rem;
 `;
 
-const MyHashTagButton = styled.button`
-    width: 7rem;
-    height: 2rem;
-    font-size: 1rem;
-    border-width: 0.1rem;
-    border-style: solid;
-    border-radius: 0.3rem;
-    background-color: white;
-    border-color: ${(props) => (props.selected ? "#47A5FD" : "#bbbbbb")};
-    color: ${(props) => (props.selected ? "#47A5FD" : "#bbbbbb")};
-    :hover {
-        border-color: #47a5fd;
-        color: #47a5fd;
-    }
-`;
-
 const MyHashTagButtonSection = ({ myHashtags }) => {
-    //내 해시태그 버튼 클릭 상태 변수
     const [selectedMyHashTagButtons, setSelectedMyHashTagButtons] = useState([]);
     const handleMyHashTagButtonClick = (index) => {
         if (selectedMyHashTagButtons.includes(index)) {
@@ -42,13 +26,13 @@ const MyHashTagButtonSection = ({ myHashtags }) => {
     return (
         <MyHashTagButtonDiv>
             {myHashtags.map((data, idx) => (
-                <MyHashTagButton
+                <HoverHashTagButton
                     key={idx}
                     selected={selectedMyHashTagButtons.includes(data)}
                     onClick={() => handleMyHashTagButtonClick(data)}
                 >
                     {data}
-                </MyHashTagButton>
+                </HoverHashTagButton>
             ))}
         </MyHashTagButtonDiv>
     );
