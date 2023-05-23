ca92e6d1410b18208958c88b0f7df3fb79c941bd
JIYun1101 <gina7440@naver.com>
2023-05-24 01:21:24 +0900

    fix: 생성 submit 제거

--- src/pages/CommunityPost/newpostpage2/newpostcontentwritingcomponents/PostCodeSection.js
+++ src/pages/CommunityPost/newpostpage2/newpostcontentwritingcomponents/PostCodeSection.js
@@ -14,51 +14,55 @@ const PostCodeSection = ({ setCode }) => {
 
   const handleSelectChange = (value) => {
     console.log(`Selected: ${value}`);
-    // 여기에서 선택된 값에 대한 처리를 할 수 있습니다.
+    setCode(value); // 선택된 값을 code 상태로 설정
   };
 
   return (
-    <Form
-      name="complex-form"
-      onFinish={() => {}}
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      style={{
-        width: "100%",
-        maxWidth: 600,
-        margin: "0 auto",
-      }}
-    >
-      <Form.Item
-        label="Select"
-        name="username"
-        noStyle
-        style={{ width: "50%", margin: "0 auto" }}
-        rules={[
-          {
-            required: true,
-            message: "Username is required",
-          },
-        ]}
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Form
+        name="complex-form"
+        onFinish={() => {}}
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          width: "100%",
+          maxWidth: 600,
+          margin: "0 auto",
+        }}
       >
-        <Select
+        <Form.Item
+          label="Select"
+          name="username"
+          noStyle
           style={{
-            width: "100%",
-            width: 200,
+            width: "50%",
           }}
-          placeholder="어떤 종류의 게시물인가요?"
-          onChange={handleSelectChange}
+          rules={[
+            {
+              required: true,
+              message: "Username is required",
+            },
+          ]}
         >
-          <Option value="1">방자랑</Option>
-          <Option value="2">정보공유</Option>
-          <Option value="3">자유</Option>
-        </Select>
-      </Form.Item>
-    </Form>
+          <Select
+            style={{
+              width: "100%",
+              width: 200,
+            }}
+            placeholder="어떤 종류의 게시물인가요?"
+            onChange={handleSelectChange}
+          >
+            <Option value="1">방자랑</Option>
+            <Option value="2">정보공유</Option>
+            <Option value="3">자유</Option>
+          </Select>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
