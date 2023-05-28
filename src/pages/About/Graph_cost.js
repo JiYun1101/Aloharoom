27b6f50b6568014336001eff48b2f7f8a600a18f
JIYun1101 <gina7440@naver.com>
2023-05-28 18:18:08 +0900

    fix: 그래프 사이값 수정

--- src/pages/About/Graph_cost.js
+++ src/pages/About/Graph_cost.js
@@ -32,22 +32,24 @@ const DemoColumn = ({ flat }) => {
       return obj;
     }, {});
 
-  const data = Object.entries(filteredDataPoints).map(([key, value]) => {
-    const [start, end] = key.slice(1).split("e"); // key 값을 분할하여 시작과 끝 값을 추출합니다.
-    const range = `${start}만원~${end}만원`; // "key(1) 평~ key(2)평" 형식으로 range 값을 생성합니다.
+  const data = Object.entries(filteredDataPoints)
+    .filter(([, value]) => value > 0) // count 값이 0보다 큰 데이터만 선택합니다.
+    .map(([key, value]) => {
+      const [start, end] = key.slice(1).split("e"); // key 값을 분할하여 시작과 끝 값을 추출합니다.
+      const range = `${start}만원~${end}만원`; // "key(1) 평~ key(2)평" 형식으로 range 값을 생성합니다.
 
-    return {
-      range,
-      count: value,
-    };
-  });
+      return {
+        range,
+        count: value,
+      };
+    });
 
   const config = {
     data: data,
     xField: "range",
     yField: "count",
-    columnWidthRatio: 0.8, // 막대의 너비 비율을 조정합니다.
-    color: (d) => (d.count === 0 ? "#DDDDDD" : "#5B8FF9"), // 0인 경우에는 회색, 아닌 경우에는 파란색으로 막대 색상을 설정합니다.
+    columnWidthRatio: 0.2, // 막대의 너비 비율을 조정합니다.
+    color: "#5B8FF9", // 막대 색상을 파란색으로 설정합니다.
     label: {
       position: "top",
       style: {
