8cc82af5a451e8be3efccf8f418a152e7ea4d5c5
gretea5 <gretea4489@gmail.com>
2023-05-30 16:34:40 +0900

    chore: 버튼 모션 커스텀

--- src/pages/HoverHashTagButton.js
+++ src/pages/HoverHashTagButton.js
@@ -7,10 +7,12 @@ const HoverHashTagButton = styled.button`
     border-width: 0.1rem;
     border-style: solid;
     border-radius: 1rem;
-    background-color: white;
-    border-color: ${(props) => (props.selected ? "#47A5FD" : "#bbbbbb")};
-    color: ${(props) => (props.selected ? "#47A5FD" : "#bbbbbb")};
+    background-color: ${(props) => (props.selected ? "#47A5FD" : "white")};
+    border-color: ${(props) => (props.selected ? "#47A5FD" : "#bbbbbb")};   
+    color: ${(props) => (props.selected ? "white" : "#bbbbbb")};
+    //color: ${(props) => (props.selected ? "white" : "#bbbbbb")};
     &:hover {
+        background-color: white;
         border-color: #47a5fd;
         color: #47a5fd;
     }
