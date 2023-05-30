4bc3a4d89e373cd9cdd253dcbf9d21ceb8737667
JIYun1101 <gina7440@naver.com>
2023-05-30 17:29:20 +0900

    feat: user 수 데이터 연동

--- src/pages/About/Graph_user.js
+++ src/pages/About/Graph_user.js
@@ -1,51 +1,82 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
 import { Line } from "@ant-design/plots";
+import axios from "axios";
+import baseURL from "../api/baseURL";
 
 const DemoLine = () => {
   const [dataPoints, setDataPoints] = useState([]);
 
-  const data = [
-    {
-      year: "11월",
-      value: 30,
-    },
-    {
-      year: "12월",
-      value: 40,
-    },
-    {
-      year: "1월",
-      value: 35,
-    },
-    {
-      year: "2월",
-      value: 51,
-    },
-    {
-      year: "3월",
-      value: 49,
-    },
-    {
-      year: "4월",
-      value: 6,
-    },
-    {
-      year: "5월",
-      value: 37,
-    },
-    {
-      year: "6월",
-      value: 29,
-    },
-    {
-      year: "7월",
-      value: 53,
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const {
+          data: { userNumIncreaseMap },
+        } = await axios.get(`${baseURL}/api/data`);
+        console.log("사람 데이터", Object.values(userNumIncreaseMap));
+        console.log("사람 데이터(서울)", Object.values(userNumIncreaseMap)[2]);
+
+        const data = [
+          {
+            year: "1월",
+            value: Object.values(userNumIncreaseMap)[0],
+          },
+          {
+            year: "2월",
+            value: Object.values(userNumIncreaseMap)[1],
+          },
+          {
+            year: "3월",
+            value: Object.values(userNumIncreaseMap)[2],
+          },
+          {
+            year: "4월",
+            value: Object.values(userNumIncreaseMap)[3],
+          },
+          {
+            year: "5월",
+            value: Object.values(userNumIncreaseMap)[4],
+          },
+          {
+            year: "6월",
+            value: Object.values(userNumIncreaseMap)[5],
+          },
+          {
+            year: "7월",
+            value: Object.values(userNumIncreaseMap)[6],
+          },
+          {
+            year: "8월",
+            value: Object.values(userNumIncreaseMap)[7],
+          },
+          {
+            year: "9월",
+            value: Object.values(userNumIncreaseMap)[8],
+          },
+          {
+            year: "10월",
+            value: Object.values(userNumIncreaseMap)[9],
+          },
+          {
+            year: "11월",
+            value: Object.values(userNumIncreaseMap)[10],
+          },
+          {
+            year: "12월",
+            value: Object.values(userNumIncreaseMap)[11],
+          },
+        ];
+
+        setDataPoints(data);
+      } catch (error) {
+        console.error("데이터를 불러오는 중에 오류가 발생했습니다.", error);
+      }
+    };
+
+    fetchData();
+  }, []); // 빈 배열을 의존성 배열로 전달하여 컴포넌트가 마운트될 때만 fetchData 함수가 실행되도록 합니다.
 
   const config = {
-    data: data,
+    data: dataPoints,
     xField: "year",
     yField: "value",
     label: {},
